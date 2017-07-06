1fdcab800888bfad0d460a6d7db10d83f23d5837
Wilmer <wilmer.ramones@gmail.com>
2017-07-06 12:22:04 -0400

    comienza a agregar la plantilla admin lte al registro de nueva persona y las clases al formtype de persona

--- src/AppBundle/Form/PersonaType.php
+++ src/AppBundle/Form/PersonaType.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,22 +20,80 @@ class PersonaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('primerNombre')
-            ->add('segundoNombre')
-            ->add('primerApellido')
-            ->add('segundoApellido')
-            ->add('cedulaPasaporte')
-            ->add('fechaNacimiento')
-            ->add('correoElectronico')
-            ->add('direccionResidencia')
+            ->add('idPaisNacimiento', EntityType::class, array(
+                'class' => 'AppBundle\Entity\PaisNacimiento',
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Pais',
+                'placeholder' => "País de Nacimiento"
+
+            ))
+            ->add('idGenero', EntityType::class, array(
+                'class' => 'AppBundle\Entity\Genero',
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Genero',
+                'placeholder' => "Seleccione Género"
+
+            ))
+
+            ->add('cedulaPasaporte', TextType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Cédula'
+            ))
+            ->add('primerNombre', TextType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+            ))
+            ->add('segundoNombre', TextType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+            ))
+            ->add('primerApellido', TextType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+            ))
+            ->add('segundoApellido', TextType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+            ))
+            ->add('fechaNacimiento', BirthdayType::class, array(
+                'attr' => array('class' => ''),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'F.Nac:'
+            ))
+            ->add('correoElectronico', EmailType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Email:'
+            ))
+            ->add('direccionResidencia', TextareaType::class, array(
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Dirección:'
+            ))
             ->add('privadoLibertad')
             ->add('certificadoConapdis')
             ->add('idSucre')
-            ->add('idPaisNacimiento')
-            ->add('idNacionalidad')
-            ->add('idGenero')
-            ->add('idEstadoCivil')
-            ->add('idDocumentoentidad')
+
+
+            ->add('idEstadoCivil', EntityType::class, array(
+                'class' => 'AppBundle\Entity\EstadoCivil',
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Genero',
+                'placeholder' => "Estado Civil"
+
+            ))
+            ->add('idDocumentoentidad', EntityType::class, array(
+                'class' => 'AppBundle\Entity\DocumentoIdentidad',
+                'attr' => array('class' => 'form-control'),
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'label' => 'Genero',
+                'placeholder' => "Tipo de Documento"
+
+            ))
             ->add('familiares', CollectionType::class, array(
                 'entry_type'   => PersonaFamiliarType::class,
                 'allow_add'    => true,
