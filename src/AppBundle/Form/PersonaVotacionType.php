14ab060e0aebb31144635b38ceae12d082a96acf
Wilmer <wilmer.ramones@gmail.com>
2017-07-12 10:31:47 -0400

    Pregunta si esta registrado en el CNE

--- src/AppBundle/Form/PersonaVotacionType.php
+++ src/AppBundle/Form/PersonaVotacionType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,14 @@ class PersonaVotacionType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('idCentroVotacion', EntityType::class, array(
+        $builder
+            ->add('cne', CheckboxType::class, array(
+                'label' => '¿Inscrito CNE?',
+                'label_attr' => array('class' => 'col-sm-3 control-label'),
+                'mapped' => false
+            ))
+
+            ->add('idCentroVotacion', EntityType::class, array(
             'class' => 'AppBundle\Entity\CentroVotacion',
             'attr' => array('class' => 'form-control'),
             'label' => 'Votación',
